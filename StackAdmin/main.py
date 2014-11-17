e85288981b7f7ed2798c39e568c576852599f7cb
rb052 <rbrixel@googlemail.com>
2014-11-17 10:47:52 +0100

    Erster GUI-Test

---
+++ StackAdmin/main.py
@@ -0,0 +1,18 @@
+import kivy
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.properties import ObjectProperty  # @UnresolvedImport
+
+class MainView(TabbedPanel):
+    lblLabel = ObjectProperty(None)
+    
+    def btnTestClick(self):
+        self.lblLabel.text = "--- Geandert! ---"
+    pass
+
+class StackAdminApp(App):
+    def build(self):
+        return MainView()
+
+if __name__ == '__main__':
+    StackAdminApp().run()

d6165f726d38fd786a0fe0d19579ee2a54c960a8
Randalinho <markusschmieder@gmx.de>
2014-12-15 00:02:25 +0100

    gitpython

--- StackAdmin/main.py
+++ StackAdmin/main.py
@@ -1,11 +1,16 @@
 #import kivy
+from git import *
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.properties import ObjectProperty  # @UnresolvedImport
 
+
 class MainView(TabbedPanel):
     lblLabel = ObjectProperty(None)
     
+    repo = Repo("/Users/mtrier/Development/git-python")
+    assert repo.bare == False
+
     def btnTestClick(self):
         self.lblLabel.text = "--- Geandert! ---"
     pass
